cb82cc335f46acccda1e817fc475aa0065f35d30
Mohammed Raza Khan <rk6265766@gmail.com>
2021-07-18 12:39:16 +0530

    Commit

--- lib/Screens/Offers/offers.dart
+++ lib/Screens/Offers/offers.dart
@@ -1,11 +1,11 @@
+import 'dart:io';
 import 'dart:typed_data';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:growbymargin_webadmin/Utils/Dimensions.dart';
 import 'package:growbymargin_webadmin/Utils/Responsive.dart';
 import 'package:growbymargin_webadmin/Utils/colors.dart';
 import 'package:sizer/sizer.dart';
@@ -20,8 +20,10 @@ class Offers extends StatefulWidget {
 
 class _OffersState extends State<Offers> {
   ConstantColors constantColors = ConstantColors();
+  TextEditingController destination = TextEditingController();
   String? offerId = Uuid().v4();
   String? imageUrl;
+
   void pickCoverImage() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
@@ -50,134 +52,133 @@ class _OffersState extends State<Offers> {
     }
   }
 
+  // FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  // FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+
+  // uploadFileToStorage(File file) {
+  //   UploadTask task = _firebaseStorage
+  //       .ref()
+  //       .child("Offers")
+  //       .child(offerId!)
+  //       .child("images/${DateTime.now().toString()}")
+  //       .putFile(file);
+  //   return task;
+  // }
+
+  // writeImageUrlToFireStore(imageUrl) {
+  //   _firebaseFirestore.collection("Offers").doc(offerId!).set({
+  //     "imgUrl": imageUrl,
+  //     "OfferId": offerId!,
+  //     // "destinationUrl": destination.text
+  //   });
+  // }
+
+  // saveImageUrlToFirebase(UploadTask task) {
+  //   task.snapshotEvents.listen((snapShot) {
+  //     if (snapShot.state == TaskState.success) {
+  //       snapShot.ref
+  //           .getDownloadURL()
+  //           .then((imageUrl) => writeImageUrlToFireStore(imageUrl));
+  //     }
+  //   });
+  // }
+
+  // Future selectFileToUpload() async {
+  //   try {
+  //     FilePickerResult? result = await FilePicker.platform
+  //         .pickFiles(allowMultiple: true, type: FileType.image);
+
+  //     if (result != null) {
+  //       selectedFiles.clear();
+
+  //       result.files.forEach((selectedFile) {
+  //         File file = File(selectedFile.path!);
+  //         selectedFiles.add(file);
+  //       });
+
+  //       selectedFiles.forEach((file) {
+  //         final UploadTask task = uploadFileToStorage(file);
+  //         saveImageUrlToFirebase(task);
+
+  //         setState(() {
+  //           uploadedTasks.add(task);
+  //         });
+  //       });
+  //     } else {
+  //       print("User has cancelled the selection");
+  //     }
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
+
+  List<UploadTask> uploadedTasks = [];
+
+  List<File> selectedFiles = [];
+
   @override
   Widget build(BuildContext context) {
+    ConstantColors constantColors = ConstantColors();
     return Scaffold(
-      backgroundColor: constantColors.whiteColor,
-      appBar: AppBar(
-          title: Text(
-            'Offers',
-            style: GoogleFonts.nunito(
-                color: Colors.black,
-                fontSize: 10.sp,
-                fontWeight: FontWeight.w600),
-          ),
-          elevation: 0,
-          backgroundColor: constantColors.whiteColor,
-          iconTheme: IconThemeData(color: constantColors.blackColor),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(EvaIcons.upload),
-              onPressed: () {},
-            )
-          ]),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 20.h,
-                  width: 30.h,
-                  child: Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              EvaIcons.image,
-                              size: 20.sp,
-                              color: constantColors.mainColor,
-                            ))
-                      ],
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.sp),
-                      color: constantColors.greyColor),
-                ),
-                hSizedBox1,
-                Container(
-                  height: 20.h,
-                  width: 30.h,
-                    child: Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              EvaIcons.image,
-                              size: 20.sp,
-                              color: constantColors.mainColor,
-                            ))
-                      ],
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.sp),
-                      color: constantColors.greyColor),
-                )
-              ],
-            ),
-            vSizedBox1,
-             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 20.h,
-                  width: 30.h,
-                  child: Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              EvaIcons.image,
-                              size: 20.sp,
-                              color: constantColors.mainColor,
-                            ))
-                      ],
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.sp),
-                      color: constantColors.greyColor),
-                ),
-                hSizedBox1,
-                Container(
-                  height: 20.h,
-                  width: 30.h,
-                    child: Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              EvaIcons.image,
-                              size: 20.sp,
-                              color: constantColors.mainColor,
-                            ))
-                      ],
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.sp),
-                      color: constantColors.greyColor),
-                )
-              ],
-            )
-          ],
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: constantColors.mainColor,
+          onPressed: () {
+          
+            // showDialog(
+            //     context: context,
+            //     builder: (contex) {
+            //       return Container(
+            //         height: 10.h,
+            //         width: 10.h,
+            //         child: Card(
+            //           child: Column(
+            //             children: [
+            //               TextField(
+            //                 controller: destination,
+            //                 decoration: InputDecoration(
+            //                   labelText: "Destination",
+            //                 ),
+            //               )
+            //             ],
+            //           ),
+            //         ),
+            //       );
+            //     });
+          },
+          child: Icon(Icons.add),
         ),
-      ),
-    );
+        backgroundColor: constantColors.whiteColor,
+        appBar: AppBar(
+            title: Text(
+              'Offers',
+              style: GoogleFonts.nunito(
+                  color: Colors.black,
+                  fontSize: 10.sp,
+                  fontWeight: FontWeight.w600),
+            ),
+            elevation: 0,
+            backgroundColor: constantColors.whiteColor,
+            iconTheme: IconThemeData(color: constantColors.blackColor),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(EvaIcons.upload),
+                onPressed: () {},
+              )
+            ]),
+        body: SingleChildScrollView(
+            child: Responsive(
+          mobile: selectedFiles.length == 0
+              ? Container(
+                  child: Center(child: Image.asset("assets/Images/offer.gif")))
+              : Container(),
+          tablet: selectedFiles.length == 0
+              ? Container(
+                  child: Center(child: Image.asset("assets/Images/offer.gif")))
+              : Container(),
+          desktop: selectedFiles.length == 0
+              ? Container(
+                  child: Center(child: Image.asset("assets/Images/offer.gif")))
+              : Container(),
+        )));
   }
 }
